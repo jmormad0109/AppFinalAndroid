16a6fd5d665b3ce7cf1dbcc605af5099ba1be8a8
JulioMoreno <juliomorenomadueno@gmail.com>
2025-01-21 22:25:55 +0100

    Arreglado NavHeader y boton LogOut al Toolbar

--- app/src/main/java/com/example/version1_1/MainActivity.kt
+++ app/src/main/java/com/example/version1_1/MainActivity.kt
@@ -2,18 +2,21 @@ package com.example.version1_1
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.recyclerview.widget.RecyclerView
-import android.app.AlertDialog
 import android.content.Context
 import android.content.Intent
 import android.content.SharedPreferences
-import android.widget.Button
-import android.widget.EditText
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.Toast
-import com.example.version1_1.databinding.ActivityLoginBinding
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.fragment.app.Fragment
 import com.example.version1_1.databinding.ActivityMainBinding
 import com.example.version1_1.fragment.ListFragment
 import com.example.version1_1.fragment.MainFragment
+import com.google.android.material.navigation.NavigationView
 import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
@@ -21,16 +24,12 @@ class MainActivity : AppCompatActivity() {
     private lateinit var sharedPreferences: SharedPreferences
     private lateinit var auth: FirebaseAuth
     private lateinit var activityMainBinding: ActivityMainBinding
-
+    private lateinit var toolbar: Toolbar
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var navView: NavigationView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        if(savedInstanceState == null){
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.fragment_container, MainFragment())
-                .commit()
-        }
 
         activityMainBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(activityMainBinding.root)
@@ -38,16 +37,74 @@ class MainActivity : AppCompatActivity() {
         sharedPreferences = getSharedPreferences("login-info", MODE_PRIVATE)
         auth = FirebaseAuth.getInstance()
 
-        activityMainBinding.logOutButton.setOnClickListener{
-            logOut(this)
+
+        if(savedInstanceState == null){
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, MainFragment())
+                .commit()
         }
 
+        toolbar = findViewById(R.id.toolbar)
+        setSupportActionBar(toolbar)
 
+        drawerLayout = findViewById(R.id.drawer_layout)
+        navView = findViewById(R.id.navigation_view)
 
+        val toggle = ActionBarDrawerToggle(
+            this, drawerLayout, toolbar,
+            R.string.navigation_drawer_open,
+            R.string.navigation_drawer_close
+        )
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
 
+        navView.setNavigationItemSelectedListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.nav_home -> navigateToFragment(MainFragment())
+                R.id.nav_profile -> navigateToFragment(ListFragment())
+                //R.id.nav_settings -> navigateToFragment(SettingsFragment())
+            }
+            // Cerrar el drawer después de seleccionar una opción
+            drawerLayout.closeDrawer(GravityCompat.START)
+            true
+        }
 
-
+        if (savedInstanceState == null) {
+            navigateToFragment(MainFragment())
+            navView.setCheckedItem(R.id.nav_home)
+        }
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.toolbar_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.log_out -> {
+                logOut(this)
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    private fun navigateToFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragment_container, fragment)
+            .commit()
+    }
+
+    override fun onBackPressed() {
+        // Cerrar el Drawer si está abierto al presionar "Atrás"
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
+    }
+
     private fun logOut(context: Context){
         FirebaseAuth.getInstance().signOut()
 
@@ -55,3 +112,4 @@ class MainActivity : AppCompatActivity() {
         startActivity(intent)
     }
 }
+
--- app/src/main/java/com/example/version1_1/fragment/MainFragment.kt
+++ app/src/main/java/com/example/version1_1/fragment/MainFragment.kt
@@ -17,6 +17,7 @@ class MainFragment: Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setHasOptionsMenu(true)
     }
 
     override fun onCreateView(
